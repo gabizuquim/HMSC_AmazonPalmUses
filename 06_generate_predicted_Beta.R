2406788264819b13db7cd16112c6284ecbe204f5
gabizuquim <gabizuquim@users.noreply.github.com>
2023-01-31 11:45:11 +0200

    Update 06_generate_predicted_Beta.R

--- 06_generate_predicted_Beta.R
+++ 06_generate_predicted_Beta.R
@@ -11,4 +11,4 @@ Fig2_data = cbind(m$TrData$use_intensity, Betameans)
 colnames(Fig2_data)[1] = "N human uses"
 colnames(Fig2_data)[-1] = paste0("Beta_", colnames(Fig2_data)[-1])
 
-Write.csv2(Fig2_data, "Fig2_data_UseIntensity_Beta.csv")
+write.csv2(Fig2_data, "Fig2_data_UseIntensity_Beta.csv")
