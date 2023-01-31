4d9299f0e88de2581b21d7fc7a9d80d2dd178883
gabizuquim <gabizuquim@users.noreply.github.com>
2023-01-31 10:57:48 +0200

    Update S5_compute_model_fit_share.R

--- S5_compute_model_fit_share.R
+++ S5_compute_model_fit_share.R
@@ -1,38 +1,30 @@
-#S5
-#setwd("") # set directory to the folder where the folders "data", "models" and "panels" are
-
-
+###METADATA####
 library(Hmsc)
+#For computing-time optimization, cross-validation was run on a model fitted with 10 x fewer samples than the version used in the rest of the paper.
+#To fit this model change the value of samples to 100 in script S3.
+load(file = "models_thin_100_samples_250_chains_4.Rdata")
 
-#You may wish to loop over samples_list and thinning_list as done in Script S3 
-#nChains = 4
-#thin = 1
-#samples = 5
-print(paste0("thin = ",as.character(thin),"; samples = ",as.character(samples)))
-filename_in = paste("models/models_thin_", as.character(thin),
-                    "_samples_", as.character(samples),
-                    "_chains_",as.character(nChains),
-                    ".Rdata",sep = "")
-load(file = filename_in) #models, modelnames
-nm = length(models)
+nChains = 4
+samples = 250
+thin = 100
 
 MF = list()
 MFCV = list()
 WAIC = list()
 
-for(model in 1:nm){
-  print(paste0("model = ",as.character(model)))
-  m = models[[model]]
+for(n in 1:1){
+  m = models[[n]]
   preds = computePredictedValues(m)
-  MF[[model]] = evaluateModelFit(hM=m, predY=preds)
-  partition = createPartition(m, nfolds = 2)
-  preds = computePredictedValues(m,partition=partition) #nParallel = nChains
-  MFCV[[model]] = evaluateModelFit(hM=m, predY=preds)
-  WAIC[[model]] = computeWAIC(m)
+  MF[[n]] = evaluateModelFit(hM=m, predY=preds)
+  partition = createPartition(m, nfolds = 4)
+  preds = computePredictedValues(m, partition=partition, nParallel = nChains)
+  MFCV[[n]] = evaluateModelFit(hM=m, predY=preds)
+  WAIC[[n]] = computeWAIC(m)       
 }
 
-filename_out = paste("models/MF_thin_", as.character(thin),
-                                            "_samples_", as.character(samples),
-                                            "_chains_",as.character(nChains),
-                                            ".Rdata",sep = "")
-save(MF,MFCV,WAIC,modelnames,file = filename_out)
+filename_out = paste("MF_models_thin_", as.character(thin),
+                     "_samples_", as.character(samples),
+                     "_chains_",as.character(nChains),
+                     ".Rdata",sep = "")
+
+save(MF, MFCV, WAIC, modelnames, file = filename_out)
