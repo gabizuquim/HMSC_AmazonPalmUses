e22051f7be26d22be397e9d38a68b7612cd2692e
gabizuquim <gabizuquim@users.noreply.github.com>
2023-01-31 11:15:15 +0200

    Update and rename 05_show_parameter_estimates_share.r to 05_generates_VarPart_values_share.r

--- 05_generates_VarPart_values_share.r
+++ 05_generates_VarPart_values_share.r
@@ -9,7 +9,6 @@ load("models_thin_1000_samples_250_chains_4.Rdata")
 
 library(Hmsc)
 
-{
   m = models[[1]]
   preds = computePredictedValues(m)
   VP = computeVariancePartitioning(m)
@@ -18,4 +17,6 @@ library(Hmsc)
   R2 = MF$TjurR2
   for(k in 1:m$ns)
   {VPr$vals[,k] = R2[k]*VPr$vals[,k]}
+  
+VPr_useintensity = data.frame(m$TrData$use_intensity,t(VPr$vals))
   
