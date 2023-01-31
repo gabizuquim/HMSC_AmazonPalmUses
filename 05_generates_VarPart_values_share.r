e4b29b68653dfb3425710aa2dc15dd1f56b0b6a4
gabizuquim <gabizuquim@users.noreply.github.com>
2023-01-31 11:45:41 +0200

    Update 05_generates_VarPart_values_share.r

--- 05_generates_VarPart_values_share.r
+++ 05_generates_VarPart_values_share.r
@@ -17,7 +17,7 @@ library(Hmsc)
 #combine the variation partitioning values with use diversity trait data for each species for figure 1
 #var part values are scaled by Tjur´s values
 VPr_useintensity = data.frame(m$TrData$use_intensity,t(VPr$vals))
-Write.csv2(VPr_useintensity, "VPr_useintensity.csv")
+write.csv2(VPr_useintensity, "VPr_useintensity.csv")
 
 #####ADD lines to create suppl Stable 1#####
 
