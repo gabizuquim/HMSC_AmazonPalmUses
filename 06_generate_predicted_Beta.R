495c8d10d353d6b2f1a0d3bc0564e6406d3f3fbc
gabizuquim <gabizuquim@users.noreply.github.com>
2023-01-31 11:42:30 +0200

    Create 06_generate_predicted_Beta.R

---
+++ 06_generate_predicted_Beta.R
@@ -0,0 +1,12 @@
+load("models_thin_1000_samples_250_chains_4.Rdata")
+
+library(Hmsc)
+
+m = models[[1]]
+postBeta = getPostEstimate(m, parName="Beta")
+Betameans = t(postBeta$mean[-1,])
+colnames(Betameans) = m$covNames[-1]
+head(m$TrData)
+Fig2_data = cbind(m$TrData$use_intensity, Betameans)
+colnames(Fig2_data)[1] = "N human uses"
+colnames(Fig2_data)[-1] = paste0("Beta_", colnames(Fig2_data)[-1])
