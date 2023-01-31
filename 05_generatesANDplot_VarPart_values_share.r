69bad78784fc59f1d981b468aaa276f45e983a75
gabizuquim <gabizuquim@users.noreply.github.com>
2023-01-31 12:12:02 +0200

    Update 05_generatesANDplot_VarPart_values_share.r

--- 05_generatesANDplot_VarPart_values_share.r
+++ 05_generatesANDplot_VarPart_values_share.r
@@ -1,4 +1,12 @@
-#METADATA
+#################################################################################################################################################
+#Codes associated to the analysis used the paper 
+#"The legacy of human use in Amazonian palm communities along environmental and accessibility gradients". 
+#Global Ecology and Biogeography. In press.
+#Codes by Otso Ovaskainen, Mirkka Jones and Gabriela Zuquim
+#The codes were modified from November 2020 Hmsc course scripts. 
+#Current versions of these training materials are available at https://www.helsinki.fi/en/researchgroups/statistical-ecology/software/hmscCodes
+#see readme file for detail
+##################################################################################################################################################
 
 library(Hmsc)
 load("models_thin_1000_samples_250_chains_4.Rdata")
