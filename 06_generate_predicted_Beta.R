beb97ff8748e3b62b660d520be87cf9970d39372
gabizuquim <gabizuquim@users.noreply.github.com>
2023-02-01 15:50:15 +0200

    Update 06_generate_predicted_Beta.R

--- 06_generate_predicted_Beta.R
+++ 06_generate_predicted_Beta.R
@@ -18,7 +18,6 @@ m = models[[1]]
 postBeta = getPostEstimate(m, parName="Beta")
 Betameans = t(postBeta$mean[-1,])
 colnames(Betameans) = m$covNames[-1]
-head(m$TrData)
 Fig2_data = cbind(m$TrData$use_intensity, Betameans)
 colnames(Fig2_data)[1] = "N human uses"
 colnames(Fig2_data)[-1] = paste0("Beta_", colnames(Fig2_data)[-1])
