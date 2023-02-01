dc2b6ec2b73aee2018cd21a483e5f804414b7ab1
gabizuquim <gabizuquim@users.noreply.github.com>
2023-02-01 11:53:03 +0200

    Update 03_evaluate_convergence_share.R

--- 03_evaluate_convergence_share.R
+++ 03_evaluate_convergence_share.R
@@ -18,24 +18,9 @@ library(sm)
 
 load("models_thin_1000_samples_250_chains_4.Rdata")
 
-nm = length(models)
-  for(j in 1:nm){
-    mpost = convertToCodaObject(models[[j]], spNamesNumbers = c(T,F), covNamesNumbers = c(T,F))
+mpost = convertToCodaObject(models[[j]], spNamesNumbers = c(T,F), covNamesNumbers = c(T,F))
     psrf.beta = gelman.diag(mpost$Beta,multivariate=FALSE)$psrf
     tmp = summary(psrf.beta)
-    if(is.null(ma)){
-      ma=psrf.beta[,1]
-      na = paste0(as.character(thin),",",as.character(samples))
-    } else {
-      ma = cbind(ma,psrf.beta[,1])
-      if(j==1){
-        na = c(na,paste0(as.character(thin),",",as.character(samples)))
-      } else {
-        na = c(na,"")
-      }
-    }
-  }
-
-par(mfrow=c(2,1))
-vioplot(ma,col=rainbow_hcl(nm),names=na,ylim=c(0,max(ma)),main="psrf(beta)")
-vioplot(ma,col=rainbow_hcl(nm),names=na,ylim=c(0.9,1.1),main="psrf(beta)")
+    ma=psrf.beta[,1]
+      
+vioplot(ma,col=rainbow_hcl(nm),ylim=c(min(ma),max(ma)),main="psrf(beta)")
